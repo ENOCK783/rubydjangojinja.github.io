63b25d5a5c990d338dcef6d9096e01b4e62e926e
ENOCK783 <enockkiptoo590@gmail.com>
2024-11-19 18:45:05 +0300

    first commit

---
+++ enockappjinja/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from enockappjinja import views
+
+
+
+urlpatterns=[
+    path('',views.home,name="my_home"),
+    path('about',views.about,name='my_about'),
+    path('services',views.services,name='my_services'),
+    path('contact', views.contact,name='my_contact')
+
+
+]
